d4a86d3978f6d60655af011804a477d8cf8fa85c
Zena AL-Jaghbeer <46792227+EngZena@users.noreply.github.com>
2021-01-31 15:13:02 +0200

    Update homepage.js

--- src/containers/mainpage/homepage.js
+++ src/containers/mainpage/homepage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Avatar from '@material-ui/core/Avatar';
-
+import TextField from '@material-ui/core/TextField';
 import classes from './homePage.module.css'
 import DatePicker from '../../component/shared/Calender/DatePicker';
 import { connect } from 'react-redux';
@@ -9,6 +9,7 @@ import Todo from '../Todo/Todo';
 
 export const HomePage = (props) => {
 
+    const [name, setName] = useState('Me');
     const [selectedDate, setSelectedDate] = useState(new Date());
     const handleDateChange = (date) => {
         setSelectedDate(date)
@@ -49,19 +50,38 @@ export const HomePage = (props) => {
 
     }
 
+    const handleChangeName = (event) => {
+        setName(event.target.value)
+    }
+
     return (
         <React.Fragment>
             <div>
+
+
                 <Avatar className={classes.pink}
                     style={{ backgroundColor: 'rgb(233, 14, 178)' }} >
-                    Z</Avatar>
-                <h2>Hey, Zaina</h2>
+                    {name ? name.slice(0, 1) : '?'}</Avatar>
+                <h2>Hey, {name ? name : 'you'}</h2>
                 <h4>Are you ready to complete your tasks?</h4>
-                <div className={classes.datePicker}>
-                    <DatePicker
-                        selectedDate={selectedDate}
-                        handleDateChange={handleDateChange}
-                    />
+                <div >
+                    <div className={classes.datePicker}>
+                        <TextField
+                            id="name"
+                            value={name}
+                            onChange={handleChangeName}
+                            label="your name"
+                            helperText="write your name"
+                            variant="outlined"
+                        />
+                    </div>
+                    <div className={classes.datePicker}>
+
+                        <DatePicker
+                            selectedDate={selectedDate}
+                            handleDateChange={handleDateChange}
+                        />
+                    </div>
                 </div>
                 {menu}
             </div>
